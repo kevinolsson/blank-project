56cbf47e50d294302d6aef992360896f9325fd3a
Kevin Olsson <kevinolsson305@gmail.com>
2022-06-15 16:08:18 +0200

    vite-plugin-sass-dts

---
+++ src/App.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const classNames: {
+  readonly app: 'app';
+  readonly appLogo: 'appLogo';
+  readonly appHeader: 'appHeader';
+  readonly appLink: 'appLink';
+  readonly counterWrapper: 'counterWrapper';
+};
+export default classNames;
+export type ClassNames = 'app' | 'appLogo' | 'appHeader' | 'appLink' | 'counterWrapper';
