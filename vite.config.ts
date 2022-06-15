030b277c04a398ea2835a2523c4090767d5f2bc6
Kevin Olsson <kevinolsson305@gmail.com>
2022-06-15 16:13:01 +0200

    vite-plugin-webfont-dl

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sassDts from 'vite-plugin-sass-dts'
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sassDts()],
+  plugins: [
+    react(),
+    sassDts(),
+    ViteWebfontDownload()
+  ],
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
